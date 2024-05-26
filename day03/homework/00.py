57dfba64ff8b670c278f2d715c7d96128ddeaff1
shotikonukradze <shotikonukradz@gmail.com>
2024-05-26 23:18:48 +0400

    second commit

---
+++ day03/homework/00.py
@@ -0,0 +1,29 @@
+print(5 + 5)
+print(5 - 5)
+print(5 * 5)
+print(5 / 5)
+
+print("goa" + " " "best")
+#aq shevaertet ori str ramac gamoitana shedegi (goa best)
+
+print(10 + "goa")
+#aq gamomitana error radgan ar shemidzlia  int s  da  str s damateba
+
+
+num1 = 10
+num2 = 5
+
+sum1 = num1 + num2
+sum2 = num1 - num2
+sum3 = num1 * num2
+sum4 = num1 / num2
+sum5 = num1 ** num2
+sum6 = num1 // num2
+
+
+print(sum1)
+print(sum2)
+print(sum3)
+print(sum4)
+print(sum5)
+print(sum6)
