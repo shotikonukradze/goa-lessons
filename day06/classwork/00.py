b991a45ec31dab3ce37dc23d2eb67f339683c330
shotikonukradze <shotikonukradz@gmail.com>
2024-05-29 20:55:45 +0400

    second

---
+++ day06/classwork/00.py
@@ -0,0 +1,21 @@
+num1 = (int(input("shemoitanet pirveli cipri: ")))
+num2 = (int(input("shemoitanet meore cipri: ")))
+num3 = (int(input("shemoitanet mesame cipri: ")))
+num4 = (int(input("shemoitanet meotxe cipri: ")))
+
+print(num1 + num2 + num3 + num4)
+print(num1 - num2 - num3 - num4)
+print(num1 * num2 * num3 * num4)
+print(num1 / num2 / num3 / num4)
+print(num1 ** num2 ** num3 ** num4)
+print(num1 // num2 // num3 // num4)
+
+
+
+
+user_firstname = input("Please enter your firstname: ")
+user_lastname = input("Please enter your lastname: ")
+user_age = input("Please enter your age: ")
+user_email = input("Please enter your email: ")
+
+print("Your inputed information is:",user_firstname,user_lastname, user_age, user_email)
