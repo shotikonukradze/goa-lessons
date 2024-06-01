a0133271312bdb11bc91c1f7c8d6e21cf803e17c
shotikonukradze <shotikonukradz@gmail.com>
2024-06-01 15:35:59 +0400

    second

---
+++ day06/homework/06.py
@@ -0,0 +1,34 @@
+#davaleba N1
+
+# num1 = (input("shoemoitanet momxmareblis saxeli: "))
+# num2 = (input("daweret misaloci werili: "))
+# num3 = "    "
+# num4 = "form "
+# num5 = " :"
+
+# print (num3)
+# print(num4 + num1 + num5 + num3 + num2)
+
+#Davaleba N2
+
+# deda = (int(input("dedis Aasaki: ")))
+# mama = (int(input("mamis Aasaki: ")))
+# da = (int(input("dis Aasaki: ")))
+
+# future = (int(input("ramdfeni wlit gsurt drois momateba: ")))
+
+
+# print(deda + future)
+# print(mama + future)
+# print(da + future)
+
+#Davaleba N3
+
+# username = input("username: ")
+# surename = input("surename: ")
+# age = (int(input("your age: ")))
+# address = input("your address: ")
+# gmail = input("your Gmail: ")
+
+# print("your information: ",username,",", surename,",",age,",", address,",", gmail)
+
