a14cd7e9688f6387b07027eaed23bc3ccf1ea8ca
shotikonukradze <shotikonukradz@gmail.com>
2024-06-12 20:55:56 +0400

    second

---
+++ day08/homework/08.py
@@ -0,0 +1,16 @@
+# age = int(input("sheiyvanet asaki: "))
+# if age <= 18 or age >= 25:
+#     print("bodishit aq ver shegishvebt")
+# else:
+#     print("mogesalmebit❤️❤️")
+
+
+# for i in range(10):
+#     print("shota")
+
+
+# budget = int(input("shemoitanet tqveni shemosavalis raodenoba: "))
+# if budget <= 50 or budget >= 100:
+#     print("error")
+# else:
+#     print("gmadlobt❤️")
---
+++ day10/classwork/00.py
@@ -0,0 +1,6 @@
+# computer = input("enter your password: ")
+
+# if computer == "luka1234":
+#     print("yochag shen warmatebit gaiare registracia💜")
+# else:
+#     print("paroli arasworia❌")
