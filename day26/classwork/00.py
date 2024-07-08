888a1ae82a8d05fea3b983bd1b7a98d089a83d28
shotikonukradze <shotikonukradz@gmail.com>
2024-07-08 23:23:50 +0400

    commit

---
+++ day26/classwork/00.py
@@ -0,0 +1,55 @@
+# Name = "gela"
+# print(Name.capitalize())
+
+# name = "shota".upper()
+# print(name)
+
+# name1 = "dzalian mixaria goashi rom var"
+# name2 = name1.count("a")
+# print(name2)
+
+# num3 = input("daweret winadadeba: ")
+# num4 = num3.lower()
+# print(num4)
+
+
+# num = "GELA ARIS kArgI BIwi"
+# num1 = num.lower()
+# print(num1)
+
+
+#find da index gamoiyeneba im shemtxvevashi roca gvinda vipovot
+#raime sityva tu romel xazzea 
+
+
+# num = "Gamrjoba Me Var Shota"
+# num1 = num.find("Shota")
+# print(num1)
+
+#find titqmis igivea rac index ubralod tu find mainc gvichvenebs integers tu arari zusad miwodebuli impormacia xolo indexs gamoaqvs error
+
+# num = "Gamrjoba Me Var Shota"
+# num1 = num.index("shota")
+# print(num1)
+
+
+
+
+# emails = []
+
+# count = int(input("Please enter how many emails do you want to input: "))
+
+# for i in range(count):
+#     email = input("Please enter email: ")
+
+#     if email.endswith("@gmail.com"):
+#         emails.append(email)
+#         print("Email was correct")
+#     else:
+#         print("Invalid email")
+
+
+# print(emails)
+
+
+
