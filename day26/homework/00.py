809e20c8aa68cd3cad7d054e95b424b90d747767
shotikonukradze <shotikonukradz@gmail.com>
2024-07-13 22:09:18 +0400

    commit

---
+++ day26/homework/00.py
@@ -0,0 +1,136 @@
+# for i in range(1, 6):
+#     print(i)
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+# for char in "hello":
+#     print(char)
+
+
+# s = "hello"
+# i = 0
+# while i < len(s):
+#     print(s[i])
+#     i += 1
+
+
+# for i in range(1, 4):
+#     print(i)
+
+
+# i = 1
+# while i <= 3:
+#     print(i)
+#     i += 1
+
+
+# for _ in range(3):
+#     print("Python is fun")
+
+
+# count = 0
+# while count < 3:
+#     print("Python is fun")
+#     count += 1
+
+
+# my_list = [1, 2, 3]
+# for element in my_list:
+#     print(element)
+
+
+
+# my_list = [1, 2, 3]
+# i = 0
+# while i < len(my_list):
+#     print(my_list[i])
+#     i += 1
+
+
+# my_list = [1, 2, 3]
+# for _ in my_list:
+#     print("hi")
+
+
+# my_list = [1, 2, 3]
+# i = 0
+# while i < len(my_list):
+#     print("hi")
+#     i += 1
+
+
+# for i in range(1, 4):
+#     print(i)
+
+
+
+# i = 1
+# while i <= 3:
+#     print(i)
+#     i += 1
+
+
+# for i in range(3, 0, -1):
+#     print(i)
+
+
+# i = 3
+# while i > 0:
+#     print(i)
+#     i -= 1
+
+
+# letters = ['a', 'b', 'c', 'd']
+# for letter in letters:
+#     print(letter)
+
+
+
+# letters = ['a', 'b', 'c', 'd']
+# i = 0
+# while i < len(letters):
+#     print(letters[i])
+#     i += 1
+
+
+
+# for _ in range(4):
+#     print("looping")
+
+
+# count = 0
+# while count < 4:
+#     print("looping")
+#     count += 1
+
+
+
+# my_tuple = (1, 2, 3)
+# for element in my_tuple:
+#     print(element)
+
+
+# my_tuple = (1, 2, 3)
+# i = 0
+# while i < len(my_tuple):
+#     print(my_tuple[i])
+#     i += 1
+
+
+# for i in range(5, 0, -1):
+#     print(i)
+
+
+# i = 5
+# while i > 0:
+#     print(i)
+#     i -= 1
+
+
+
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
---
+++ day27/homework/00.py
@@ -0,0 +1,17 @@
+# # num = int(input("gtovt shemoitanot ricxvi: "))
+# # num1 = 5
+# # print(num + num1)
+
+
+
+# name = int(input("saxeli: "))
+# surename = input("gvari: ")
+
+
+# print("sheni saxeli aris: ", + name)
+
+
+name = int(input("asaki: "))
+num1 = name + 36
+
+print("sheni asaki 36 wlis shemdeg: ", + num1)
